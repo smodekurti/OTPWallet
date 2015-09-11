2bd17f2930ba365fd382f9117f1f2e6a706f610e
smodekurti <gitvasu@gmail.com>
2015-09-11 16:11:29 -0500

    Updated edit button

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,7 +9,9 @@ angular.module('starter')
                                        TotpFactory,
                                        $ionicPopup,
                                        $ionicModal,
-                                        $state){
+                                        $state,
+                                       QRScanService,
+                                       $ionicActionSheet){
 
     $scope.current =        0;
     $scope.max =            30;
@@ -91,16 +93,7 @@ angular.module('starter')
 
 
 
-    $scope.edit=function(alias){
-        var key = keyService.getKey(alias);
-
-
-        $scope.modal.show().then(function(modal){
-            //console.log("In Scope Modal");
-            $scope.newKey=key;
-        });
-
-        $scope.createOrUpdateAccount = function(){
+    $scope.createOrUpdateAccount = function(){
 
             var base32String = null;
             try {
@@ -134,11 +127,77 @@ angular.module('starter')
             }
 
 
-        }
+        };
+
+
+    function extractUrlValue(key, url)
+    {
+        if (typeof(url) === 'undefined')
+            url = window.location.href;
+        var match = url.match('[?&]' + key + '=([^&]+)');
+        return match ? match[1] : null;
+    }
+
+    $scope.edit = function(alias) {
+        var key = keyService.getKey(alias);
+        // Show the action sheet
+        var hideSheet = $ionicActionSheet.show({
+            buttons: [
+                { text: '<i class="icon fa fa-qrcode fa-fw"></i> Scan Barcode' },
+                { text: '<i class="icon fa fa-pencil-square-o fa-fw"></i>  Enter Key Manually' }
+            ],
+            titleText: 'Setup Options',
+            cancelText: 'Cancel',
+            cancel: function() {
+                // add cancel code..
+            },
+            buttonClicked: function(index) {
+                switch(index){
+                    case 0:
+
+                        QRScanService.scan(function(result) {
+                            if (result.cancelled) {
+                                // this is a super hack. When QR scan gets cancelled by
+                                // clicking the back button on android, the app quits...
+                                // doing a blank modal to catch the back button press event
+                                $ionicModal.fromTemplate('').show().then(function() {
+                                    $ionicPopup.alert({
+                                        title: 'QR Scan Cancelled',
+                                        template: 'No Code could be scanned since user canceled scanning!'
+                                    });
+                                });
+                            }
+                            else {
+                                var secretParam = extractUrlValue('secret',result.text);
+
+                                $scope.newKey.secret = secretParam;
+                                $scope.modal.show().then(function(modal){
+                                    //console.log("In Scope Modal");
+                                });
+                            }
+                        }, function(error) {
+                            $ionicPopup.alert({
+                                title: 'Unable to scan the QR code',
+                                template: 'Too bad, something went wrong.'
+                            });
+                        });
+
+                        break;
+                    case 1:
+                        $scope.newKey = {};
+                        $scope.modal.show().then(function(modal){
+                            $scope.newKey=key;
+                        });
+                }
+                return true;
+            }
+        });
+
+
     };
 
     $scope.delete=function(alias){
-        console.log(alias);
+
         var confirmPopup = $ionicPopup.confirm({
             title: 'Delete Totp Key',
             template: 'Are you sure you want to delete this key ?'
