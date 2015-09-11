811800c99a41437bb2872f4b716c59f1291bf160
smodekurti <hivasu@gmail.com>
2015-09-11 12:45:21 -0500

    Added delete button

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -302,8 +302,7 @@ angular.module('starter')
     }
 
     $scope.newKey  = {alias:'',secret:'',timeBased:'true'};
-
-        $scope.editOrNew = false;
+    $scope.editOrNew = false;
     /* Modal Popup Definitions*/
 
     $ionicModal.fromTemplateUrl(
